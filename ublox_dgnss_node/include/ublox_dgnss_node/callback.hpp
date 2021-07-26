2a009e5e2c3740c68159fa4d0854a1384fb33068
Nick Hortovanyi <nick.hortovanyi@gmail.com>
2021-07-26 15:39:54 +1000

    uncrustify

--- ublox_dgnss_node/include/ublox_dgnss_node/callback.hpp
+++ ublox_dgnss_node/include/ublox_dgnss_node/callback.hpp
@@ -17,64 +17,72 @@
 #include <stdio.h>
 #include <functional>
 
-template <typename T>
+template<typename T>
 struct callback_in_t;
 
-template <typename Ret, typename... Params>
-struct callback_in_t<Ret(Params...)> {
-   template <typename... Args>
-   static Ret callback(Args... args) {
-      return func(args...);
-   }
-   static std::function<Ret(Params...)> func;
+template<typename Ret, typename ... Params>
+struct callback_in_t<Ret(Params...)>
+{
+  template<typename ... Args>
+  static Ret callback(Args... args)
+  {
+    return func(args ...);
+  }
+  static std::function<Ret(Params...)> func;
 };
 
-template <typename Ret, typename... Params>
+template<typename Ret, typename ... Params>
 std::function<Ret(Params...)> callback_in_t<Ret(Params...)>::func;
 
-template <typename T>
+template<typename T>
 struct callback_out_t;
 
-template <typename Ret, typename... Params>
-struct callback_out_t<Ret(Params...)> {
-   template <typename... Args>
-   static Ret callback(Args... args) {
-      return func(args...);
-   }
-   static std::function<Ret(Params...)> func;
+template<typename Ret, typename ... Params>
+struct callback_out_t<Ret(Params...)>
+{
+  template<typename ... Args>
+  static Ret callback(Args... args)
+  {
+    return func(args ...);
+  }
+  static std::function<Ret(Params...)> func;
 };
 
-template <typename Ret, typename... Params>
+template<typename Ret, typename ... Params>
 std::function<Ret(Params...)> callback_out_t<Ret(Params...)>::func;
 
-template <typename T>
+template<typename T>
 struct hotplug_detach_callback_t;
 
-template <typename Ret, typename... Params>
-struct hotplug_detach_callback_t<Ret(Params...)> {
-   template <typename... Args>
-   static Ret callback(Args... args) {
-      return func(args...);
-   }
-   static std::function<Ret(Params...)> func;
+template<typename Ret, typename ... Params>
+struct hotplug_detach_callback_t<Ret(Params...)>
+{
+  template<typename ... Args>
+  static Ret callback(Args... args)
+  {
+    return func(args ...);
+  }
+  static std::function<Ret(Params...)> func;
 };
 
-template <typename Ret, typename... Params>
+template<typename Ret, typename ... Params>
 std::function<Ret(Params...)> hotplug_detach_callback_t<Ret(Params...)>::func;
 
-template <typename T>
+template<typename T>
 struct hotplug_attach_callback_t;
 
-template <typename Ret, typename... Params>
-struct hotplug_attach_callback_t<Ret(Params...)> {
-   template <typename... Args>
-   static Ret callback(Args... args) {
-      return func(args...);
-   }
-   static std::function<Ret(Params...)> func;
+template<typename Ret, typename ... Params>
+struct hotplug_attach_callback_t<Ret(Params...)>
+{
+  template<typename ... Args>
+  static Ret callback(Args... args)
+  {
+    return func(args ...);
+  }
+  static std::function<Ret(Params...)> func;
 };
 
-template <typename Ret, typename... Params>
+template<typename Ret, typename ... Params>
 std::function<Ret(Params...)> hotplug_attach_callback_t<Ret(Params...)>::func;
 
 #endif  // UBLOX_DGNSS_NODE__CALLBACK_HPP_
